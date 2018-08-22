907cbfcb319d0972b76a5c2107c17e77db7a8608
metawin99 <pai.pp61@gmail.com>
2018-08-22 11:49:50 +0700

    Pai init

--- VideoPicker/ViewController.swift
+++ VideoPicker/ViewController.swift
@@ -7,11 +7,25 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    let imagePickerController = UIImagePickerController()
+    var videoURL: NSURL?
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tapGestureRecognizer)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +34,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func previewImageFromVideo(url:NSURL) -> UIImage? {
+        let asset = AVAsset(url: url as URL)
+        let imageGenerator = AVAssetImageGenerator(asset:asset)
+        imageGenerator.appliesPreferredTrackTransform = true
+        
+        var time = asset.duration
+        time.value = min(time.value,2)
+        
+        do {
+            let imageRef = try imageGenerator.copyCGImage(at: time, actualTime: nil)
+            return UIImage(cgImage: imageRef)
+        } catch {
+            return nil
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        videoURL = info[UIImagePickerControllerMediaURL] as? NSURL
+        imageView.image = previewImageFromVideo(url: videoURL!)!
+        imageView.contentMode = .scaleAspectFit
+        imagePickerController.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        imagePickerController.mediaTypes = ["public.movie"]
+        //imagePickerController.mediaTypes = ["public.image", "public.movie"]
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        imagePickerController.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        if let videoURL = videoURL{
+            let player = AVPlayer(url: videoURL as URL)
+            let playerViewController = AVPlayerViewController()
+            playerViewController.player = player
+            present(playerViewController, animated: true){
+                playerViewController.player!.play()
+            }
+        }
+    }
+    
 
 }
 
